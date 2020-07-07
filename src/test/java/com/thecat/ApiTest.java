caf1eadb4b88f17559598c963fea33a7a65f760e
Gilson Silva <gilsonsilvati@gmail.com>
2020-07-06 23:12:35 -0300

    Melhorando nosso código

--- src/test/java/com/thecat/ApiTest.java
+++ src/test/java/com/thecat/ApiTest.java
@@ -3,81 +3,63 @@ package com.thecat;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 
-public class ApiTest {
+public class ApiTest extends MassaDeDados {
 
-    private static String URL_BASE = "https://api.thecatapi.com/v1/";
-
-    private String path;
-    private String vote_id;
+    @BeforeClass
+    public static void setUpClass() {
+        baseURI = "https://api.thecatapi.com/v1/";
+    }
 
     @Test
     public void deve_cadastrar() {
-        path = new StringBuilder(URL_BASE).append("user/passwordlesssignup").toString();
-        String corpo = "{\"email\" : \"leka.silva@gmail.com\", \"appDescription\" : \"Teste The Cat API -> Gilson Silva\"}";
-
         // GIVEN -> DADO QUE / WHEN -> QUANDO ESTIVER COM / THEN -> ENTÃO
         Response response = given()
                 .contentType(ContentType.JSON)
-                .body(corpo)
+                .body(corpoCadastro)
             .when()
-                .post(path);
+                .post(pathCadastro);
 
-        response
-            .then()
-                .statusCode(200);
-
-        assertThat(response.getStatusCode(), is(equalTo(200)));
+        validacao(response);
     }
 
     @Test
     public void deve_votar() {
-        path = new StringBuilder(URL_BASE).append("votes/").toString();
-
         // GIVEN -> DADO QUE / WHEN -> QUANDO ESTIVER COM / THEN -> ENTÃO
         Response response = given()
                 .contentType(ContentType.JSON)
-                .body("{\"image_id\" : \"MjA0NzcwNA\", \"value\" : true, \"sub_id\" : \"demo-b6f1ef\"}")
+                .body(corpoVotacao)
             .when()
-                .post(path);
+                .post("votes/");
 
-        response
-            .then()
-                .statusCode(200);
+        id = response.jsonPath().getString("id");
 
-        System.out.println(">>> Response : " + response.body().asString());
-        vote_id = response.jsonPath().getString("id");
-        System.out.println(">>> ID : " + vote_id);
+        validacao(response);
     }
 
     @Test
     public void deve_deletar_voto() {
         deve_votar();
-        deve_deletar();
+//        deve_deletar();
     }
 
     private void deve_deletar() {
-        path = new StringBuilder(URL_BASE).append("votes/{vote_id}").toString();
-
         // GIVEN -> DADO QUE / WHEN -> QUANDO ESTIVER COM / THEN -> ENTÃO
         Response response = given()
                 .contentType(ContentType.JSON)
                 .header(new Header("x-api-key", "d56b216d-3a9a-4d4c-821e-8dabeb5ee0ca"))
-                .pathParam("vote_id", vote_id)
+                .pathParam("vote_id", id)
             .when()
-                .delete(path);
+                .delete("votes/{vote_id}");
 
-        System.out.println(">>> Response : " + response.body().asString());
-
-        response
-            .then()
-                .statusCode(200)
-                .body("message", equalTo("SUCCESS"));
+        validacao(response);
     }
 
     @Test
@@ -87,43 +69,37 @@ public class ApiTest {
     }
 
     private void deve_favoritar() {
-        path = new StringBuilder(URL_BASE).append("favourites").toString();
-
         // GIVEN -> DADO QUE / WHEN -> QUANDO ESTIVER COM / THEN -> ENTÃO
         Response response = given()
                 .contentType(ContentType.JSON)
                 .header(new Header("x-api-key", "d56b216d-3a9a-4d4c-821e-8dabeb5ee0ca"))
-                .body("{\"image_id\" : \"2rl\"}")
+                .body(corpoFavorita)
             .when()
-                .post(path);
+                .post("favourites");
 
-        System.out.println(">>> Response favorita : " + response.body().asString());
-        vote_id = response.jsonPath().getString("id");
+        id = response.jsonPath().getString("id");
 
-        response
-            .then()
-                .statusCode(200)
-                .body("message", equalTo("SUCCESS"));
+        validacao(response);
     }
 
     private void deve_desfavoritar() {
-        path = new StringBuilder(URL_BASE).append("favourites").append("/{favourite_id}").toString();
-
         // GIVEN -> DADO QUE / WHEN -> QUANDO ESTIVER COM / THEN -> ENTÃO
         Response response = given()
                 .contentType(ContentType.JSON)
                 .header(new Header("x-api-key", "d56b216d-3a9a-4d4c-821e-8dabeb5ee0ca"))
-                .pathParam("favourite_id", vote_id)
+                .pathParam("favourite_id", id)
             .when()
-                .delete(path);
+                .delete(corpoDesfavorita);
 
-        System.out.println(">>> Response desfavorita : " + response.body().asString());
-        System.out.println("ID : " + vote_id);
+        validacao(response);
+    }
 
-        response
-            .then()
-                .statusCode(200)
-                .body("message", equalTo("SUCCESS"));
+    private void validacao(Response response) {
+        response.then().statusCode(200).body("message", is(equalTo("SUCCESS")));
+
+        System.out.println(">>> Retorno da API : " + response.body().asString());
+        System.out.println("ID : " + id);
+        System.out.println("-----------------------------------------------------------------------");
     }
 
 }
---
+++ src/test/java/com/thecat/MassaDeDados.java
@@ -0,0 +1,12 @@
+package com.thecat;
+
+public class MassaDeDados {
+
+    String id;
+    String pathCadastro = "user/passwordlesssignup";
+    String corpoCadastro = "{\"email\" : \"leka.silva@gmail.com\", \"appDescription\" : \"Teste The Cat API -> Gilson Silva\"}";
+    String corpoVotacao = "{\"image_id\" : \"MjA0NzcwNA\", \"value\" : true, \"sub_id\" : \"demo-b6f1ef\"}";
+    String corpoFavorita = "{\"image_id\" : \"2rl\"}";
+    String corpoDesfavorita = "favourites/{favourite_id}";
+
+}
